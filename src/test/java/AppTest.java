d649d9afa432e53728a37f32dfafc60f13f1ec44
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-05-30 10:12:21 +0900

    add test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -10,6 +10,11 @@ public class AppTest {
     @Test public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-        assertThat("messageCheck", classUnderTest.getGreeting(), Is.is("Hello world.aa"));
+        assertThat("messageCheck", classUnderTest.getGreeting(), Is.is("Hello world."));
+    }
+    @Test public void test02() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+        assertThat("messageCheck", classUnderTest.getGreeting(), Is.is("Hello world."));
     }
 }
